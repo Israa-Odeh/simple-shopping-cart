632b43bde061ac3a7c435dc77d9685ede64329f1
Israa <Israa.K.Odeh@hotmail.com>
2024-11-13 17:11:17 +0200

    Add images for different products

--- src/data/products.js
+++ src/data/products.js
@@ -1,75 +1,111 @@
+import ceramicBowls from "images/products/ceramicBowls.jpg";
+import pinkGoldGlasses from "images/products/pinkGoldGlasses.jpg";
+import grayCanisters from "images/products/grayCanisters.jpg";
+import rattanRoundBags from "images/products/rattanRoundBags.jpg";
+import woodenJournalNotebook from "images/products/woodenJournalNotebook.jpg";
+import lavenderBlossomSoap from "images/products/lavenderBlossomSoap.jpg";
+import rosewoodSoapBar from "images/products/rosewoodSoapBar.jpg";
+import solidCeramicVase from "images/products/solidCeramicVase.jpg";
+import miniVases from "images/products/miniVases.jpg";
+import pinkCeramicVase from "images/products/pinkCeramicVase.jpg";
+import silkCareshampoo from "images/products/silkCareshampoo.jpg";
+import wallDecor from "images/products/wallDecor.jpg";
+
 const products = [
   {
     id: 1,
-    name: "Blue Denim Jacket",
-    description:
-      "Stylish blue denim jacket with a classic fit, featuring button closures and chest pockets.",
-    imageUrl: "https://picsum.photos/seed/picsum/200/",
-    price: 59066.99,
+    name: "Ceramic Bowls",
+    description: "Five round white ceramic bowls on white surface.",
+    imageUrl: ceramicBowls,
+    price: 25,
   },
   {
     id: 2,
-    name: "Red T-shirt",
+    name: "Pink & Gold Glasses",
     description:
-      "100% cotton t-shirt, comfortable and breathable, available in sizes S, M, L, XL.",
-    imageUrl: "https://picsum.photos/200",
-    price: 19.99,
+      "Stylish pink and gold glasses with a modern design and UV protection.",
+    imageUrl: pinkGoldGlasses,
+    price: 100,
   },
   {
     id: 3,
-    name: "Leather Boots",
+    name: "Gray Canister Set",
     description:
-      "High-quality leather boots with durable soles, perfect for outdoor adventures.",
-    imageUrl: "https://picsum.photos/200",
-    price: 129.99,
+      "A sleek set of two gray canisters, perfect for storing kitchen essentials with a modern, minimalist design.",
+    imageUrl: grayCanisters,
+    price: 10,
   },
   {
     id: 4,
-    name: "Sports Watch",
+    name: "Rattan Round Bag",
     description:
-      "Water-resistant sports watch with a digital display and heart-rate monitor.",
-    imageUrl: "https://picsum.photos/200/",
-    price: 89.99,
+      "Stylish round rattan bags, handmade for a relaxed, casual look.",
+    imageUrl: rattanRoundBags,
+    price: 20,
   },
   {
     id: 5,
-    name: "Sports Watch",
+    name: "Wooden Journal Notebook",
     description:
-      "Water-resistant sports watch with a digital display and heart-rate monitor.",
-    imageUrl: "https://picsum.photos/200/",
-    price: 89.99,
+      "A unique notebook with a wood-textured cover, perfect for writing, sketching, or note-taking.",
+    imageUrl: woodenJournalNotebook,
+    price: 10,
   },
   {
     id: 6,
-    name: "Sports Watch",
+    name: "Lavender Blossom Soap",
     description:
-      "Water-resistant sports watch with a digital display and heart-rate monitor.",
-    imageUrl: "https://picsum.photos/200/",
-    price: 89.99,
+      "A soothing lavender-scented soap that gently cleanses and nourishes your skin, leaving it feeling soft and refreshed.",
+    imageUrl: lavenderBlossomSoap,
+    price: 5,
   },
   {
     id: 7,
-    name: "Sports Watch",
+    name: "Rosewood Soap Bar",
     description:
-      "Water-resistant sports watch with a digital display and heart-rate monitor.",
-    imageUrl: "https://picsum.photos/200/",
-    price: 89.99,
+      "A luxurious soap with a rich rosewood scent, designed to hydrate and rejuvenate your skin while providing a calming, refreshing aroma.",
+    imageUrl: rosewoodSoapBar,
+    price: 6,
   },
   {
     id: 8,
-    name: "Sports Watch",
+    name: "Solid Ceramic Vase",
     description:
-      "Water-resistant sports watch with a digital display and heart-rate monitor.",
-    imageUrl: "https://picsum.photos/200/",
-    price: 89.99,
+      "A stylish, solid ceramic vase, ideal for placing your favorite flowers or as a decorative piece on its own.",
+    imageUrl: solidCeramicVase,
+    price: 15,
   },
   {
     id: 9,
-    name: "Sports Watch",
+    name: "Two-Tone Mini Vase",
     description:
-      "Water-resistant sports watch with a digital display and heart-rate monitor.",
-    imageUrl: "https://picsum.photos/200/",
-    price: 89.99,
+      "Mini vase with a wheat top and matte pink bottom, perfect for any space.",
+    imageUrl: miniVases,
+    price: 5,
+  },
+  {
+    id: 10,
+    name: "Baby Pink Ceramic Vase",
+    description:
+      "Elegant medium-sized vase in soft baby pink, perfect for placing flowers or as a decorative accent in any room.",
+    imageUrl: pinkCeramicVase,
+    price: 10,
+  },
+  {
+    id: 11,
+    name: "SilkCare Nourishing Shampoo",
+    description:
+      "Hydrating shampoo infused with natural ingredients to cleanse and nourish your hair.",
+    imageUrl: silkCareshampoo,
+    price: 12,
+  },
+  {
+    id: 12,
+    name: "Round Wooden Wall Decor",
+    description:
+      "A rustic brown round wooden wall decor, perfect for adding a natural, warm touch to any room.",
+    imageUrl: wallDecor,
+    price: 50,
   },
 ];
 
